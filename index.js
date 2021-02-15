882ca7cbd85c4f2d0a8d4ddcce8f282bd3c7de08
Zahra <hizahrazadeh@gmail.com>
2021-02-15 16:27:37 -0500

    u

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get();
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+//implement a couple of end points that respond to http get request 
+// "slash" / represents the root of the function
+//second arg is a callback function, this is the callback that will be called
+//when there's an http request to this end point
+//the callback function is also called a route handler
